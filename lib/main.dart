e95da88edac282ce088148218036fca6c75286a7
Andrierlison <andrierlisonsilva8@gmail.com>
2022-11-26 20:39:48 -0300

    Added sentry to project

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,13 @@ import 'package:flutter/services.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
-
+import 'package:leitor_qr_code/sentry.dart';
 // Project imports:
 import 'package:leitor_qr_code/ui/global_styles.dart';
 import 'package:leitor_qr_code/ui/screens/home_screen.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   // Hide status bar
@@ -19,24 +20,30 @@ void main() {
 
   MobileAds.instance.initialize();
 
-  runApp(
-    MaterialApp(
-      showSemanticsDebugger: false,
-      showPerformanceOverlay: false,
-      debugShowCheckedModeBanner: false,
-      theme: appTheme,
-      localizationsDelegates: const [
-        AppLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: const [
-        Locale('en', ''),
-        Locale('es', ''),
-        Locale('pt', ''),
-      ],
-      home: const HomeScreen(),
+  await SentryFlutter.init(
+    (options) {
+      options.dsn = sentryKey;
+      options.tracesSampleRate = 1.0;
+    },
+    appRunner: () => runApp(
+      MaterialApp(
+        showSemanticsDebugger: false,
+        showPerformanceOverlay: false,
+        debugShowCheckedModeBanner: false,
+        theme: appTheme,
+        localizationsDelegates: const [
+          AppLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: const [
+          Locale('en', ''),
+          Locale('es', ''),
+          Locale('pt', ''),
+        ],
+        home: const HomeScreen(),
+      ),
     ),
   );
 }
---
+++ lib/sentry.dart
@@ -0,0 +1,2 @@
+const String sentryKey =
+    'https://738d2651cced42e8ad81e832d9c12d85@o1357289.ingest.sentry.io/4504228045455360';
