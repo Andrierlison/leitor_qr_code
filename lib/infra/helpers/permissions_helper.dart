2bdeff4ac79eb1e00639cf8a889b803364bdc928
Andrierlison <andrierlisonsilva8@gmail.com>
2022-11-26 16:55:45 -0300

    Created helper to manage device permissions

---
+++ lib/infra/helpers/permissions_helper.dart
@@ -0,0 +1,13 @@
+import 'package:permission_handler/permission_handler.dart';
+
+class PermissionsHelper {
+  Future<void> requestStorage() async {
+    Permission storagePermission = Permission.storage;
+    await storagePermission.request();
+  }
+
+  Future<bool> canAccessStorage() async {
+    PermissionStatus result = await Permission.storage.status;
+    return result.isGranted ? true : false;
+  }
+}
