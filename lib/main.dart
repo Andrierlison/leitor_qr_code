8d841c3c5bf01633530c9e7032e6a1ba0fabf687
Andrierlison <andrierlisonsilva8@gmail.com>
2022-11-26 16:56:09 -0300

    Hide status bar

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,14 @@ import 'package:leitor_qr_code/ui/global_styles.dart';
 import 'package:leitor_qr_code/ui/screens/home_screen.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Hide status bar
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
+
   MobileAds.instance.initialize();
 
   runApp(
