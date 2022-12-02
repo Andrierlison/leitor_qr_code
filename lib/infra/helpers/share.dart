39dd27d1aec27fcde9510a2ef950e60c6b6876ff
Andrierlison <andrierlisonsilva8@gmail.com>
2022-12-02 07:10:46 -0300

    Share files at share.dart (helper)

--- lib/infra/helpers/share.dart
+++ lib/infra/helpers/share.dart
@@ -1,7 +1,17 @@
+// Package imports:
 import 'package:share_plus/share_plus.dart';
 
 class ShareHelper {
   void text({required String text}) {
     Share.share(text);
+  }
+
+  void files({required List<String> files}) {
+    try {
+      List<XFile> toShare = files.map((item) => XFile(item)).toList();
+      Share.shareXFiles(toShare);
+    } catch (error) {
+      throw const FormatException('Error');
+    }
   }
 }
